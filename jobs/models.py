712aa40569bd37c36e4db4debaed1fe3a1281cb1
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2020-03-05 14:50:17 -0300

    initial project portfolio

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    image_intro = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=255)
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        db_table = 'job'
+        managed = True
+        verbose_name = 'Job'
+        verbose_name_plural = 'Jobs'
