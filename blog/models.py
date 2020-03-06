a7a78d46d92fd30714440391875047e91467e75e
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2020-03-05 21:08:57 -0300

    finishing homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    summary = models.TextField()
+    intro_image = models.ImageField(upload_to="blog/")
+    content = models.TextField()
+    published = models.BooleanField(default=True)
+    publish_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+    
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+
+from .models import Job
 
 # Create your views here.
+
+def home(request: HttpRequest):
+    jobs = list(Job.objects.all())
+    print(jobs)
+    
+    return render(request, "jobs/home.html", {"jobs": jobs})
